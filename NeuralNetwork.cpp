1b3b954e6493ccb8445ab10653fd32b4f9c8c6d4
zyouyowa <transam3y@gmail.com>
2015-07-13 09:27:13 +0900

    change members' & methods' names.  add comments
    yurusite

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -6,7 +6,7 @@
 #define COUNT_INPUT 4
 
 int main(int argc, char* argv[]){
-	TinyNeuralNetwork tnn(2, 2, 1, 0.1);
+	TinyNeuralNetwork tnn(2, 4, 1, 0.1);
 	std::vector< std::vector<double> > input;
 	input.resize(COUNT_INPUT);
 	for (int i = 0; i < COUNT_INPUT; ++i){
@@ -19,11 +19,11 @@ int main(int argc, char* argv[]){
 	}
 
 	input[0][0] = 0.0; input[0][1] = 0.0; teacher[0][0] = 0.0;
-	input[1][0] = 0.0; input[1][1] = 1.0; teacher[1][0] = 0.0;
-	input[2][0] = 1.0; input[2][1] = 0.0; teacher[2][0] = 0.0;
-	input[3][0] = 1.0; input[3][1] = 1.0; teacher[3][0] = 1.0;
+	input[1][0] = 0.0; input[1][1] = 1.0; teacher[1][0] = 1.0;
+	input[2][0] = 1.0; input[2][1] = 0.0; teacher[2][0] = 1.0;
+	input[3][0] = 1.0; input[3][1] = 1.0; teacher[3][0] = 0.0;
 
-	for (int i = 0; i < 10000; ++i){	//42
+	for (int i = 0; i < 1000; ++i){
 		for (int j = 0; j < COUNT_INPUT; ++j){
 			tnn.ForwardPropagation(input[j]);
 			tnn.BackPropagation(teacher[j]);
--- TNN.h
+++ TNN.h
@@ -2,7 +2,16 @@
 #include "math.h"
 #include "vector"
 
-inline double randDouble(){
+/* TODO:
+ * ・ForwardPropagationの引数を"配列の先頭のポインタ？"、返り値を"結果の配列？"
+ * ・BackPropagationの引数を??
+ * ・各メソッドの引数、返り値を決める
+ * ・どんなメソッドが必要か？
+ * ・コメントの追加
+ */
+
+//0.0~1.0の間の値を返す関数
+inline double rand_from0to1(){
 	return (double)rand() / ((double)RAND_MAX);
 }
 
@@ -10,135 +19,144 @@ inline double sigmoid(double x){
 	return 1 / (1 + exp(-x));
 }
 
-inline double defferentialSigmoid(double x){
-	return sigmoid(x) * (1 - sigmoid(x));
+//引数にはsigmoid(x)の返り値を入れる
+inline double defferentialSigmoid(double y){
+	return y * (1 - y);
 }
-inline double dSig_already(double x){
-	return x * (1 - x);
-}
+/* 参考
+ * 
+ * y = σ(x) = 1 / (1 + exp(-x))
+ * σ'(x) = σ(x) * (1 - σ(x)) = y * (1 - y)
+*/
+
+/*
+ * アルゴリズムは<http://www-ailab.elcom.nitech.ac.jp/lecture/neuro/menu.html>のバックプロパゲーションを参照
+ * 参考:	<https://github.com/uyu-kickstart/tiny-nn/blob/master/src/neuralnetwork.js>
+ * 		<http://edof.hatenablog.com/entry/2015/05/11/161342>
+ * 
+*/
 
 class TinyNeuralNetwork{
 	/*
-	 * in 		: 入力層
-	 * hidden 	: 隠れ層
-	 * out 		: 出力層
+	 * X 		: 入力層
+	 * H 		: 隠れ層
+	 * O 		: 出力層
 	 * 
-	 * output 	: 出力
 	 * weight 	: 重み
-	 * bias 	: バイアス、閾値
-	 * alpha 	: 重みの変化分の係数
-	 * eta 		: 最急降下法の係数
+	 * alpha 	: 学習係数
+	 * 
+	 * count_unit_ : 各層のunit数
 	*/
 	public:
-		int count_unit_in;
-		int count_unit_hidden;
-		int count_unit_out;
-		double alpha;
-		std::vector< std::vector<double> > weight_in_to_hidden;		//weight_in_to_hidden[count_unit_in][count_unit_hidden]
-		std::vector< std::vector<double> > weight_hidden_to_out; 
+		int count_X;
+		int count_H;
+		int count_O;
+		double alp;
+		std::vector< std::vector<double> > weight_X_to_H;
+		std::vector< std::vector<double> > weight_H_to_O; 
 
-		std::vector<double> output_in;					//入力するのはここに入れる
-		std::vector<double> output_hidden;
-		std::vector<double> output_out;
+		std::vector<double> X;					//入力するのはここに入れる
+		std::vector<double> H;
+		std::vector<double> O;
 
 		/*
-		 * weight_in_to_hidden,weight_hidden_to_out,output_in,output_hiddenにはバイアスの分が入る
+		 * weight_X_to_H,weight_H_to_O,X,Hにはバイアスの分が入る
 		*/
 
 
-		TinyNeuralNetwork(int unit_in, int unit_hidden, int unit_out, double alp){
-			count_unit_in = unit_in;			//0番目のニューロンの入力は常に1,結合荷重は-θ
-			count_unit_hidden = unit_hidden;
-			count_unit_out = unit_out;
-			alpha = alp;
+		TinyNeuralNetwork(int unit_in, int unit_hidden, int unit_out, double alpha){
+			count_X = unit_in;
+			count_H = unit_hidden;
+			count_O = unit_out;
+			alp = alpha;
 
-			int count_unit_in_weight = count_unit_in + 1;
-			weight_in_to_hidden.resize(count_unit_in_weight);		//+1はバイアスの分
-			for(int i = 0; i < count_unit_in_weight; ++i){
-				weight_in_to_hidden[i].resize(count_unit_hidden);
+			int count_X_p1 = count_I + 1;			//weightの数は
+			weight_X_to_H.resize(count_X_p1);
+			for(int i = 0; i < count_X_p1; ++i){
+				weight_X_to_H[i].resize(count_H);
 			}
-			int count_unit_hidden_weight = count_unit_hidden + 1;
-			weight_hidden_to_out.resize(count_unit_hidden_weight);
-			for(int i = 0; i < count_unit_hidden_weight; ++i){
-				weight_hidden_to_out[i].resize(count_unit_out);
+			int count_H_p1 = count_H + 1;
+			weight_H_to_O.resize(count_H_p1);
+			for(int i = 0; i < count_H_p1; ++i){
+				weight_H_to_O[i].resize(count_O);
 			}
-			output_in.resize(count_unit_in);
-			output_hidden.resize(count_unit_hidden);
-			output_out.resize(count_unit_out);
+			X.resize(count_X);
+			H.resize(count_H);
+			O.resize(count_O);
 
 			Initialize();
 		}
 
 		void Initialize (){
-			int count_unit_in_weight = count_unit_in + 1;
+			int count_X_p1 = count_X + 1;
 			srand((unsigned)time(NULL));
 			//重み、閾値の初期化。乱数を突っ込む
-			for (int i = 0; i < count_unit_in_weight; ++i){
-				for (int j = 0; j < count_unit_hidden; ++j){
-					weight_in_to_hidden[i][j] = randDouble();
+			for (int i = 0; i < count_X_p1; ++i){
+				for (int j = 0; j < count_H; ++j){
+					weight_X_to_H[i][j] = rand_from0to1();
 				}
 			}
-			int count_unit_hidden_weight = count_unit_hidden + 1;
-			for (int j = 0; j < count_unit_hidden_weight; ++j){
-				for (int k = 0; k < count_unit_out; ++k){
-					weight_hidden_to_out[j][k] = randDouble();
+			int count_H_p1 = count_H + 1;
+			for (int j = 0; j < count_H_p1; ++j){
+				for (int k = 0; k < count_O; ++k){
+					weight_H_to_O[j][k] = rand_from0to1();
 				}
 			}
 		}
 
 		void ForwardPropagation(std::vector<double> &input){
 			double sum;
-			output_in = input;
+			X = input;
 			//入力層->隠れ層の伝搬
-			for (int j = 0; j < count_unit_hidden; ++j){
+			for (int j = 0; j < count_H; ++j){
 				sum = 0.0;
-				for (int i = 0; i < count_unit_in; ++i){
-					sum += weight_in_to_hidden[i][j] * output_in[i];
+				for (int i = 0; i < count_X; ++i){
+					sum += weight_X_to_H[i][j] * X[i];
 				}
-				sum += weight_in_to_hidden[count_unit_in][j] * 1;
-				output_hidden[j] = sigmoid(sum);		//名前によって関数のポインタをさせば？
+				sum += weight_X_to_H[count_X][j] * 1;
+				H[j] = sigmoid(sum);		//名前によって関数のポインタをさせば？
 			}
 			//隠れ層->出力層の伝搬
-			for (int k = 0; k < count_unit_out; ++k){
+			for (int k = 0; k < count_O; ++k){
 				sum = 0.0;
-				for (int j = 0; j < count_unit_hidden; ++j){
-					sum += weight_hidden_to_out[j][k] * output_hidden[j];
+				for (int j = 0; j < count_H; ++j){
+					sum += weight_H_to_O[j][k] * H[j];
 				}
-				sum += weight_hidden_to_out[count_unit_hidden][k] * 1;
-				output_out[k] = sigmoid(sum);
+				sum += weight_H_to_O[count_H][k] * 1;
+				O[k] = sigmoid(sum);
 			}
 		}
+
 		void BackPropagation(std::vector<double> &teacher_signal){
 			double sum = 0.0;
+			std::vector<double> delta_H;
+			delta_H.resize(count_H);
 
-			std::vector<double> delta_output_hidden;
-			delta_output_hidden.resize(count_unit_hidden);
-
-			std::vector<double> delta_output_out;
-			delta_output_out.resize(count_unit_out);
+			std::vector<double> delta_O;
+			delta_O.resize(count_O);
 			
-			for (int k = 0; k < count_unit_out; ++k){
-				delta_output_out[k] = (teacher_signal[k] - output_out[k]) * dSig_already(output_out[k]);
+			for (int k = 0; k < count_O; ++k){
+				delta_O[k] = (teacher_signal[k] - O[k]) * defferentialSigmoid(O[k]);
 			}
 
-			for (int j = 0; j < count_unit_hidden; ++j){
+			for (int j = 0; j < count_H; ++j){
 				sum = 0.0;
-				for (int k = 0; k < count_unit_out; ++k){
-					sum += weight_hidden_to_out[j][k] * delta_output_out[k];
+				for (int k = 0; k < count_O; ++k){
+					sum += weight_H_to_O[j][k] * delta_O[k];
 				}
-				delta_output_hidden[j] = dSig_already(output_hidden[j]) * sum;
+				delta_H[j] = defferentialSigmoid(H[j]) * sum;
 
-				for (int i = 0; i < count_unit_in; ++i){
-					weight_in_to_hidden[i][j] += alpha * delta_output_hidden[j] * output_in[i];
+				for (int i = 0; i < count_X; ++i){
+					weight_X_to_H[i][j] += alpha * delta_H[j] * X[i];
 				}
-				weight_in_to_hidden[count_unit_in][j] += alpha * delta_output_hidden[j] * 1.0;
+				weight_X_to_H[count_X][j] += alpha * delta_H[j] * 1.0;
 			}
 
-			for (int k = 0; k < count_unit_out; ++k){
-				for (int j = 0; j < count_unit_hidden; ++j){
-					weight_hidden_to_out[j][k] += alpha * delta_output_out[k] * output_hidden[j];
+			for (int k = 0; k < count_O; ++k){
+				for (int j = 0; j < count_H; ++j){
+					weight_H_to_O[j][k] += alpha * delta_O[k] * H[j];
 				}
-				weight_hidden_to_out[count_unit_hidden][k] += alpha * delta_output_out[k] * 1.0;
+				weight_H_to_O[count_H][k] += alpha * delta_O[k] * 1.0;
 			}
 		}
 };
